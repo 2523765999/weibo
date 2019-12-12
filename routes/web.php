c56fedba4211e26869bec233c7d1920cda1d28f3
songdexin <2523765999@qq.com>
2019-12-12 18:20:43 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
-Route::get('signup','UserController@create')->name('signup');
+Route::get('signup','UsersController@create')->name('signup');
+// Route::Resource('users','UsersController');//必须是复数形式的，Route::Resource('user','UserController')会报错。
+Route::resource('users', 'UsersController');
