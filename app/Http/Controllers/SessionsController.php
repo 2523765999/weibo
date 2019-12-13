6fee7fe71b09d9614881c0f7c1c41ff108f1f1ab
songdexin <2523765999@qq.com>
2019-12-13 15:53:53 +0800

    增加记住我功能

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -18,7 +18,7 @@ class SessionsController extends Controller
             'email' => 'required|max:255',
             'password' => 'required'
         ]);
-        if (Auth::attempt($credentials)){
+        if (Auth::attempt($credentials, $request->has('remember'))){
             session()->flash('success','欢迎回来');
             return redirect()->route('users.show',[Auth::user()]);//获取当前登录用户信息
         } else {
