cdf63aaa0dce187d37ddb3d185d693b97c2d2e3a
songdexin <2523765999@qq.com>
2019-12-13 15:38:37 +0800

    登录成功后，导航更改以及注册后自动登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -20,7 +20,7 @@ class SessionsController extends Controller
         ]);
         if (Auth::attempt($credentials)){
             session()->flash('success','欢迎回来');
-            return redirect('users.show',[Auth::user()]);//获取当前登录用户信息
+            return redirect()->route('users.show',[Auth::user()]);//获取当前登录用户信息
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();//失败后，withInput能将填写信息返回到old内
