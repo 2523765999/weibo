c7ffb76588fc850530cb17ccadf2af0f20a34fb7
songdexin <2523765999@qq.com>
2019-12-17 20:40:43 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,10 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * Class User
+ * @package App\Models
+ */
 class User extends Authenticatable
 {
     use Notifiable;
@@ -37,20 +41,63 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    /**
+     * @param string $size
+     * @return string
+     */
     public function gravatar($size='100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public  function statuses()
     {
         return $this->hasMany(Status::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function feed()
     {
         return $this->statuses()
             ->orderBy('created_at', 'desc');
     }
+    //获取粉丝关系列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'followers','user_id', 'follower_id');
+        //第三个参数：模型外键名，  第四个参数：要合并的模型外键名
+    }
+    //获取用户关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+    //加关注
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+    //取消关注
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+    
+    public function isFollow($user_id)
+    {
+        $this->followings()->contains($user_id);
+    }
+
 }
